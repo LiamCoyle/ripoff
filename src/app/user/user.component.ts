9f69f34ec08aa893edaaf1c2b12d5a652355fb9b
LiamCoyle <32932501+LiamCoyle@users.noreply.github.com>
2018-10-12 10:05:36 +0200

    commit 1
    commit 1

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { UserService } from '../user.service';
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  users: User[];
+
+  constructor(private userService: UserService) { }
+
+  getUsers(): void {
+    this.users = this.UserService.getUsers();
+  }
+
+  ngOnInit() {
+  }
+
+}
