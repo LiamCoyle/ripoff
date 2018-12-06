d069de035305e9ec7f6f2629c058b7be0629176d
LiamCoyle <zynk123@hotmail.com>
2018-12-06 12:50:38 +0100

    clean

--- src/app/models/product.ts
+++
@@ -1,7 +0,0 @@
-export class Product {
-    _id: number;
-    name: string;
-    menu_id : number;
-    url: string;
-    prix: number;
-}
--- src/app/models/user.ts
+++
@@ -1,5 +0,0 @@
-export class User {
-    _id: number;
-    name: string;
-    password : string;
-}
